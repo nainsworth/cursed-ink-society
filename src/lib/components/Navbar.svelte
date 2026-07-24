3a7bcee35c110ce055f0515caf72d73b0b848e97
nainsworth <nja8161995@gmail.com>
2026-07-23 22:50:08 -0500

    Build navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { resolve } from '$app/paths';
+</script>
+
+<nav class="py-4 px-10">
+	<div class="flex justify-between items-center">
+		<a href={resolve('/')} class="font-anton text-2xl text-primary">CURSED INK SOCIETY</a>
+
+		<ul class="flex gap-4 font-space-mono text-sm">
+			<li><a href={resolve('/')}>HOME</a></li>
+			<li><a href="/artists">ARTISTS</a></li>
+			<li><a href="/tattoos">TATTOOS</a></li>
+			<li><a href="/piercings">PIERCINGS</a></li>
+			<li><a href="/aftercare">AFTERCARE</a></li>
+			<li><a href="/faq">FAQ</a></li>
+			<li><a href="/contact" class="btn-primary py-1 px-3 hover:text-background!">CONTACT</a></li>
+		</ul>
+	</div>
+</nav>
+
+<style>
+	
+        a:hover {
+            color: var(--color-primary);
+            
+        }
+        
+    
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import Navbar from '$lib/components/Navbar.svelte';
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 
+<Navbar />
 <main>
 	{@render children()}
 </main>
