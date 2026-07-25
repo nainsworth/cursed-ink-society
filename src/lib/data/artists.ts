7308f453894cd341bc02e3fcaf40091f218d78a6
nainsworth <nja8161995@gmail.com>
2026-07-25 17:38:16 -0500

    Created artists data file

---
+++ src/lib/data/artists.ts
@@ -0,0 +1,84 @@
+import profileAngelle from '../assets/images/profile-angelle.png';
+import profileAnthony from '../assets/images/profile-anthony.png';
+import profileAshley from '../assets/images/profile-ashley.png';
+import profileCody from '../assets/images/profile-cody.png';
+import profileHarley from '../assets/images/profile-harley.png';
+import profileLuis from '../assets/images/profile-luis.png';
+import profileNadiya from '../assets/images/profile-nadiya.png';
+import profileSeth from '../assets/images/profile-seth.png';
+
+export interface Artist {
+	slug: string;
+	name: string;
+	image: string;
+	specialties: string[];
+	bio: string;
+	instagram: string;
+}
+
+export const artists: Artist[] = [
+	{
+		slug: 'angelle',
+		name: 'Angelle',
+		image: profileAngelle,
+		specialties: [],
+		bio: '',
+		instagram: 'https://instagram.com/angelle_tattoos'
+	},
+	{
+		slug: 'anthony',
+		name: 'Anthony',
+		image: profileAnthony,
+		specialties: [],
+		bio: '',
+		instagram: 'https://instagram.com/buddha_ta2'
+	},
+	{
+		slug: 'ashley',
+		name: 'Ashley',
+		image: profileAshley,
+		specialties: [],
+		bio: '',
+		instagram: 'https://instagram.com/ashleyxxkarma'
+	},
+	{
+		slug: 'cody',
+		name: 'Cody',
+		image: profileCody,
+		specialties: [],
+		bio: '',
+		instagram: 'https://instagram.com/codycrochettattoos'
+	},
+	{
+		slug: 'harley',
+		name: 'Harley',
+		image: profileHarley,
+		specialties: [],
+		bio: '',
+		instagram: 'https://instagram.com/harley.rose25'
+	},
+	{
+		slug: 'luis',
+		name: 'Luis',
+		image: profileLuis,
+		specialties: [],
+		bio: '',
+		instagram: 'https://instagram.com/ta2luis'
+	},
+	{
+		slug: 'nadiya',
+		name: 'Nadiya',
+		image: profileNadiya,
+		specialties: [],
+		bio: '',
+		instagram: 'https://instagram.com/electricsting'
+	},
+	{
+		slug: 'seth',
+		name: 'Seth',
+		image: profileSeth,
+		specialties: [],
+		bio: '',
+		instagram: 'https://instagram.com/sethta2_bp'
+	}
+];
