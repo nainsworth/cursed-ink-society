ee086e6c4f19bb952edaa1492482698ad02ae324
nainsworth <nja8161995@gmail.com>
2026-07-25 13:19:03 -0500

    Finished styling for hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,23 +13,28 @@
 		class="absolute inset-0 h-full w-full object-cover"
 	></video>
 
-	<div class="absolute inset-0 flex items-center bg-black/80">
-		<div class="mt-12 space-y-4">
+	<div class="absolute inset-0 flex items-center gradient">
+		<div class="mt-12 space-y-4 m-10">
 			<p class="font-space-mono text-xs tracking-[0.28em]">HOUMA, LOUISIANA</p>
-			<h1 class="font-anton text-9xl leading-28 text-primary">TATTOOS <br /> <span>& PIERCINGS</span></h1>
-            <!-- <p class="font-archivo max-w-md text-[#b8b5b0] leading-[1.6]">Seven artist and a resident piercer in the heart of Houma. Walk-ins welcome or find your artist and book directly.</p> -->
-            <div class='space-x-2 mt-6'>
-                <button class="btn-primary spac">BOOK AN ARTIST</button>
-                <button class="btn-secondary">TATTOO GALLERY</button>
-            </div>
+			<h1 class="font-anton text-9xl leading-28 text-primary">
+				TATTOOS <br /> <span>& PIERCINGS</span>
+			</h1>
+			<p class="font-archivo max-w-md text-[#b8b5b0] leading-[1.6]">Seven artist and a resident piercer in the heart of Houma. Come as a walk-in or find your artist and book directly.</p>
+			<div class="mt-12 space-x-2">
+				<a href='#' class="btn-primary spac">BOOK AN ARTIST</a>
+				<a href="#" class="btn-secondary">TATTOO GALLERY</a>
+			</div>
 		</div>
 	</div>
-
 </section>
 
 <style>
-    span {
-        color: transparent;
-        -webkit-text-stroke: 6px var(--color-primary);
-    }
+	span {
+		color: transparent;
+		-webkit-text-stroke: 6px var(--color-primary);
+	}
+
+	.gradient {
+		background: linear-gradient(rgba(11, 11, 11, 0.5), rgba(11, 11, 11, 0.95));
+	}
 </style>
