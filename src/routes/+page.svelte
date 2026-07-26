91f7b32290c6b6026bfa0a92481cc63eecf95525
nainsworth <nja8161995@gmail.com>
2026-07-25 21:50:24 -0500

    Added a hover effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,24 +3,32 @@
 	import Marquee from '$lib/components/Marquee.svelte';
 
 	import { artists } from '$lib/data/artists';
+	import { resolve } from '$app/paths';
 </script>
 
 <Hero />
 <Marquee />
 
-<section class="p-10">
+<section class="px-10 py-20">
 	<div class="mx-auto max-w-7xl space-y-8">
-		<h2 class="font-anton text-5xl text-primary uppercase">The Artists</h2>
+		<h2 class="font-anton text-6xl text-primary uppercase">The Artists</h2>
 
 		<div class="flex gap-6">
 			{#each artists.filter((a) => a.role === 'Artist' || a.role === 'Apprentice') as artist (artist.slug)}
-				<div>
-					<img src={artist.image} alt={artist.name} class="h-128 w-full object-cover" />
-				</div>
+				<a href={resolve('/')} class="text-center font-space-mono uppercase transition-transform duration-100 hover:scale-110">
+					<img
+						src={artist.image}
+						alt={artist.name}
+						class="h-128 w-full object-cover hover:drop-shadow-lg hover:drop-shadow-primary/50"
+					/>
+					<div class="mt-4 ">
+						<p class="text-sm font-bold text-primary">{artist.name}</p>
+						<p class="text-xs">{artist.role}</p>
+					</div>
+				</a>
 			{/each}
 		</div>
 
 		<div class="grid grid-cols-2 gap-4 md:grid-cols-4"></div>
 	</div>
 </section>
-
